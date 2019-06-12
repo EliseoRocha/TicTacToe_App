dd2bd0a55887a5c0817b878fc1e9eb4e768187e0
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-06-11 17:00:50 -0700

    Working on bug fixes with toolbar

---
+++ app/src/main/java/com/example/tictactoe/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.tictactoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
